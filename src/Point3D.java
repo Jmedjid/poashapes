236bfcf4fcf0d1c98fd825ca80b0229c4948a4a8
Joan <j.medjid@orange.fr>
2016-10-23 16:07:02 +0200

    The API for shapes

--- src/Point3D.java
+++ src/Point3D.java
@@ -5,26 +5,60 @@ public class Point3D extends Point2D {
 
     private double z;
 
+    public Point3D() {
+        this(0,0,0);
+    }
 
-    public boolean isOrigin() {
-        if (super.isOrigin() && (this.z == 0.0)){
-            return true;
-        }
-        else{
-            return false;
-        }
+    public Point3D(double x, double y, double z) {
+        super(x, y);
+        this.z = z;
     }
 
 
     public void translate(double dx, double dy, double dz) {
         super.translate(dx, dy);
-        this.z = dz;
+        this.z = z + dz;
     }
 
+    public boolean isOrigin() {
+        return super.isOrigin() && this.z == 0.0;
+    }
 
     public double distance(Point3D other) {
-        return Math.sqrt(Math.pow((super.x - other.x), 2.0)+Math.pow((super.y - other.y), 2.0)+(Math.pow((this.z - other.z), 2.0)));
+        return Math.sqrt(Math.pow(other.getX() - getX(), 2) + Math.pow(other.getY() - getY(), 2) + Math.pow(other.getZ() - getZ(), 2));
     }
 
+    public double getZ() {
+        return z;
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Point3D)) return false;
+        if (!super.equals(o)) return false;
+
+        Point3D point3D = (Point3D) o;
+
+        return Double.compare(point3D.getZ(), getZ()) == 0;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        long temp;
+        temp = Double.doubleToLongBits(z);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Point3D{" +
+                "x=" + getX() +
+                "y=" + getY() +
+                "z=" + z +
+                '}';
+    }
 }
